6ccdec2ee80e6f6e21d5528385b9ca181ea16b3a
yersultanzada <yersultan.zada@gmail.com>
2019-09-19 19:31:00 +0600

    ...

--- js/project.js
+++ js/project.js
@@ -34,4 +34,76 @@
     offset: 56
   });
 
+    $("iframe[data-src]").Lazy();
+
+    $('.team-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        centerMode: true,
+        variableWidth: true,
+        infinite: true,
+        focusOnSelect: true,
+        cssEase: 'linear',
+        touchMove: true,
+        prevArrow:'<button class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow:'<button class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+
+        //         responsive: [
+        //             {
+        //               breakpoint: 576,
+        //               settings: {
+        //                 centerMode: false,
+        //                 variableWidth: false,
+        //               }
+        //             },
+        //         ]
+    });
+
+    $('.gallery-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        centerMode: true,
+        verticalSwiping: true,
+        variableWidth: true,
+        infinite: true,
+        focusOnSelect: true,
+        cssEase: 'linear',
+        touchMove: true,
+        asNavFor: '.gallery-slider-nav',
+        prevArrow:'<button class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow:'<button class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+
+        //         responsive: [
+        //             {
+        //               breakpoint: 576,
+        //               settings: {
+        //                 centerMode: false,
+        //                 variableWidth: false,
+        //               }
+        //             },
+        //         ]
+    });
+
+    $('.gallery-slider-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.gallery-slider',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+    $('.testimonials-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow:'<button class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow:'<button class="slick-next"><i class="fas fa-chevron-right"></i></button>'
+    });
+
 })(jQuery); // End of use strict
